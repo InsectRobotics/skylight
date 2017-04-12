5011ece18880da51d3ce1310789c2e008c38810f
Evripidis Gkanias <ev.gkanias@gmail.com>
2017-04-12 18:07:14 +0100

    create a sky model of luminance and simple polarisation

---
+++ sky/model.py
@@ -0,0 +1,158 @@
+import numpy as np
+import healpy as hp
+import ephem
+from datetime import datetime
+from .utils import *
+
+
+class SkyModel(object):
+    NSIDE = 32
+    VIEW_ROT = (0, 90, 0)
+    a_default = -1.
+    b_default = -.55
+    c_default = 10.
+    d_default = -3.
+    e_default = .45
+    alpha_default = -132.1
+    beta_default = 59.77
+
+    def __init__(self, observer=None, sun=ephem.Sun(), nside=NSIDE):
+        self.sun = sun
+        self.obs = observer
+        if observer is None:
+            self.obs = ephem.city("Edinburgh")
+            self.obs.date = datetime(2017, 6, 21, 0, 0, 0)
+
+        # calculate the pixel indices
+        i = np.arange(hp.nside2npix(nside))
+        # get the longitude and co-latitude with respect to the zenith
+        self.theta, self.phi = hp.pix2ang(nside, i)        # return longitude and co-latitude in radians
+        # we initialise the sun at the zenith
+        # so the angular distance between the sun and every point is equal to their distance from the zenith
+        self.theta_s, self.phi_s = self.theta.copy(), self.phi.copy()
+
+        # initialise the scattering indicatrix with zeros
+        self.si = np.zeros_like(self.theta_s)   # scattering indicatrix
+        self.lg = np.zeros_like(self.theta)     # luminance gradation
+        self.L = np.zeros_like(self.theta)      # total luminance
+        self.T = np.zeros_like(self.theta)      # colour temperature
+
+        # initialise the polarization features
+        self.DOP = np.zeros_like(self.theta)    # Degree of Polarisation
+        self.AOP = np.zeros_like(self.theta)    # Angle of Polarisation
+
+    def generate(self, show=False):
+        # update the relevant sun position
+        self.sun.compute(self.obs)
+
+        # calculate the angular distance between the sun and every point on the map
+        lon, lat = sun2lonlat(self.sun, show=show)
+        x, y, z = 0, np.rad2deg(lat), 180 + np.rad2deg(lon)
+        self.theta_s, self.phi_s = hp.Rotator(rot=(z, y, x))(self.theta, self.phi)
+        self.theta_s, self.phi_s = self.theta_s % np.pi, self.phi_s % (2 * np.pi)
+
+        if show:
+            import matplotlib.pyplot as plt
+
+            plt.figure(1, figsize=(15, 5))
+            hp.orthview(self.theta, rot=self.VIEW_ROT, min=0, max=np.pi, flip="geo", cmap="Greys", half_sky=True,
+                        title="Elevation", unit=r'rad', sub=(1, 4, 1), fig=1)
+            hp.orthview(self.phi, rot=self.VIEW_ROT, min=0, max=2 * np.pi, flip="geo", cmap="Greys", half_sky=True,
+                        title="Azimuth", unit=r'rad', sub=(1, 4, 2), fig=1)
+            hp.orthview(self.theta_s, rot=self.VIEW_ROT, min=0, max=np.pi, flip="geo", cmap="Greys", half_sky=True,
+                        title="Elevation", unit=r'rad', sub=(1, 4, 3), fig=1)
+            hp.orthview(self.phi_s, rot=self.VIEW_ROT, min=0, max=2 * np.pi, flip="geo", cmap="Greys", half_sky=True,
+                        title="Azimuth", unit=r'rad', sub=(1, 4, 4), fig=1)
+            hp.projplot(lat, lon, 'yo')
+
+        # calculate luminance of the sky
+        self.si = self.scattering_indicatrix(self.theta_s)
+        self.lg = self.luminance_gradation(self.theta)
+        self.L = self.luminance(self.theta_s, self.theta)
+        self.T = self.colour_temperature(self.L)
+
+        if show:
+            plt.figure(2, figsize=(15, 5))
+            hp.orthview(self.si, rot=self.VIEW_ROT, min=0, max=10, flip="geo", cmap="Greys", half_sky=True,
+                        title="Scattering indicatrix", unit=r'', sub=(1, 4, 1), fig=2)
+            hp.orthview(self.lg, rot=self.VIEW_ROT, min=0, max=1, flip="geo", cmap="Greys", half_sky=True,
+                        title="Luminance gradation", unit=r'Cd/m^2', sub=(1, 4, 2), fig=2)
+            hp.orthview(self.L, rot=self.VIEW_ROT, min=0, max=5.6, flip="geo", cmap="Greys", half_sky=True,
+                        title="Luminance", unit=r'Cd/m^2', sub=(1, 4, 3), fig=2)
+            hp.orthview(self.T, rot=self.VIEW_ROT, min=0, max=257, flip="geo", cmap="Greys", half_sky=True,
+                        title="Colour temperature", unit=r'MK^(-1)', sub=(1, 4, 4), fig=2)
+            hp.projplot(lat, lon, 'yo')
+
+        # calculate the polarisation features
+        self.DOP = 2 * rayleigh(self.theta_s)
+        self.AOP = (self.phi_s + np.pi/2) % np.pi
+
+        if show:
+            plt.figure(3, figsize=(15, 5))
+            hp.orthview(self.DOP, rot=self.VIEW_ROT, min=0, max=1, flip="geo", cmap="Greys", half_sky=True,
+                        title="Degree of (linear) Polarisation", unit=r'', sub=(1, 2, 1), fig=3)
+            hp.orthview(self.AOP, rot=self.VIEW_ROT, min=0, max=np.pi, flip="geo", cmap="Greys", half_sky=True,
+                        title="Angle of (linear) Polarisation", unit=r'rad', sub=(1, 2, 2), fig=3)
+            hp.projplot(lat, lon, 'yo')
+
+            plt.show()
+
+    @classmethod
+    def luminance(cls, x, z, a=a_default, b=b_default, c=c_default, d=d_default, e=e_default):
+        """
+        Combines the scattering indicatrix and luminance gradation functions to compute the total luminance observed at
+        the given sky element(s).
+        
+        :param x: angular distance between the observed element and the sun [0, pi]
+        :param z: angular distance between the observed element and the zenith [0, pi/2]
+        :param a: scalar -- affects the amplitude of the curve (luminance gradation)
+        :param b: scalar -- affects the curvature of the curve (luminance gradation)
+        :param c: scalar -- affects the amplitude of the exponential component (scattering indicatrix)
+        :param d: scalar -- affects the curvature of the exponential component (scattering indicatrix)
+        :param e: scalar -- affects the amplitude of the sinusoidal component (scattering indicatrix)
+        :return:  the total observed luminance (Cd/m^2) at the given element(s)
+        """
+        return cls.scattering_indicatrix(x, c, d, e) * cls.luminance_gradation(z, a, b)
+
+    @classmethod
+    def luminance_gradation(cls, z, a=a_default, b=b_default):
+        """
+        The luminance gradation function relates the luminance of a sky element to its zenith angle.
+        
+        :param z: angular distance between the observed element and the zenith -- [0, pi/2]
+        :param a: scalar -- affects the amplitude of the curve
+        :param b: scalar -- affects the curvature of the curve
+        :return:  the observed luminance gradation (Cd/m^2) at the given element(s) -- [0, 1] for default parameters
+        """
+        phi = np.zeros_like(z)
+        # apply border conditions to avoid dividing with zero
+        z_p = np.all([z >= 0, z < np.pi / 2], axis=0)
+        phi[z_p] = 1. + a * np.exp(b / np.cos(z[z_p]))
+        phi[np.isclose(z, np.pi/2)] = 1.
+        return phi
+
+    @classmethod
+    def scattering_indicatrix(cls, x, c=c_default, d=d_default, e=e_default):
+        """
+        The scattering indicatrix which relates the relative luminance of the sky element
+        to its angular distance from the sun.
+        
+        :param x: angular distance between the observed element and the sun -- [0, pi]
+        :param c: scalar -- affects the amplitude of the exponential component
+        :param d: scalar -- affects the curvature of the exponential component
+        :param e: scalar -- affects the amplitude of the sinusoidal component
+        :return:  the observed scattering indicatrix at the given element(s) -- [0, inf) for default parameters
+        """
+        return 1. + c * (np.exp(d * x) - np.exp(d * np.pi/2)) + e * np.square(np.cos(x))
+
+    @classmethod
+    def colour_temperature(cls, L, alpha=alpha_default, beta=beta_default):
+        """
+        The temperature of the colour in the given sky element(s)
+        
+        :param L: the observed luminance
+        :param alpha: constant shifting of temperature
+        :param beta: linear transformation of the temperature
+        :return: the temperature of the colour (MK^(-1)) with respect to its luminance
+        """
+        return -alpha + beta * np.log(L)
---
+++ sky/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def sun2lonlat(s, lonlat=False, show=False):
+    lon, lat = s.az, s.alt
+    colat = np.pi / 2 - lat
+
+    if lon > np.pi:
+        lon -= 2 * np.pi.
+
+    if show:
+        print('Sun:\tLon = %.2f\t Lat = %.2f\t Co-Lat = %.2f' % \
+                (np.rad2deg(lon), np.rad2deg(lat), np.rad2deg(colat)))
+
+    if lonlat:  # return the longitude and the latitude in degrees
+        return np.rad2deg(lon), np.rad2deg(lat)
+    else:  # return the lngitude and the co-latitude in radians
+        return lon, colat
+
+def hard_sigmoid(x, s=10):
+    return 1. / (1. + np.exp(-s * x))
+
+def rayleigh(x, sigma=np.pi/2):
+    # make sure the input is not negative
+    x = np.absolute(x)
+    return (x / np.square(sigma)) * np.exp(-np.square(x) / (2 * np.square(sigma)))
