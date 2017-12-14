Fix rotation of the sky to work on 3D - fix bugs in sensor lenses orientation - update compound eyes and sensor to tilt correctly - finalise tilting and star creating datasets for tilting

tests/simulate-skylight.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
 
-
+# W = H = 1000
 sun = Sun()
 
 if __name__ == '__main__':
@@ -63,11 +63,11 @@ if __name__ == '__main__':
         x, y = x[j], y[j]
         theta, phi = theta[j], phi[j]
         sky.get_features(theta, phi)
-        sky = sky.rotate_sky(sky, yaw=90, pitch=60, roll=-60)
+        sky = sky.rotate_sky(sky, yaw=np.pi/2, pitch=np.pi/6, roll=np.pi/3)
         sky.generate()
 
         if mode == 0:
-            sph = np.stack([sky.theta, sky.phi, np.ones_like(sky.theta)])
+            sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
             image = np.zeros((W, H, 3))
             sky.DOP[np.isnan(sky.DOP)] = -1
             i = np.argsort(sky.DOP)
@@ -85,7 +85,7 @@ if __name__ == '__main__':
             # SkyModel.plot_luminance(sky, fig=1, title="Luminance", mode="00100", sub=(1, 1, 1))
         elif mode == 1:
 
-            sph = np.stack([sky.theta, sky.phi, np.ones_like(sky.theta)])
+            sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
             image = np.zeros((W, H, 3))
             x, y = sph2pix(sph, W, H)
             sky.DOP[np.isnan(sky.DOP)] = 0.

tests/test-sky.py
@@ -47,8 +47,8 @@ for tau in xrange(1, 11):
     print ""
     # for i, s in enumerate(sky.S):
     #     S = np.array([sky.W, s]).T
-    #     title = "Theta: %0.2f, Phi: %0.2f" % (sky.theta[i], sky.phi[i])
-    #     name = "theta_%02d-phi_%03d" % (np.rad2deg(sky.theta[i]), np.rad2deg(sky.phi[i]))
+    #     title = "Theta: %0.2f, Phi: %0.2f" % (sky.theta_z[i], sky.phi_z[i])
+    #     name = "theta_%02d-phi_%03d" % (np.rad2deg(sky.theta_z[i]), np.rad2deg(sky.phi_z[i]))
     #     spectrum_plot(S, title, "%s.png" % name)
     #     rgb = rgb_from_xyz(xyz_from_spectrum(S))
     #     print rgb