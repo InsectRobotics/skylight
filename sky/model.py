Fix rotation of the sky to work on 3D - fix bugs in sensor lenses orientation - update compound eyes and sensor to tilt correctly - change the whitening method of the sensor from PCA to ZCA - test that the sensor rotates properly - create new data using the fixed sensor

sky/model.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 # from colorpy.rayleigh import rayleigh_illuminated_spectrum
 # from colorpy.illuminants import
 from datetime import datetime
+from numbers import Number
 from .utils import *
 
 
@@ -445,8 +446,17 @@ class SkyModel(object):
         return f
 
     @staticmethod
-    def rotate_sky(sky, yaw=0., pitch=0., roll=0.):
-        sky.theta_z, sky.phi_z = SkyModel.rotate(sky.theta_z, sky.phi_z, yaw=yaw, pitch=pitch, roll=roll)
+    def rotate_sky(sky, yaw=0., pitch=0., roll=0., zenith=True):
+        if zenith:
+            sky.theta_z, sky.phi_z = SkyModel.rotate(sky.theta_z, sky.phi_z, yaw=yaw, pitch=pitch, roll=roll)
+        else:
+            theta = np.pi / 2 - sky.theta_z
+            phi = np.pi - sky.phi_z
+
+            theta, phi = SkyModel.rotate(theta, phi, yaw=yaw, pitch=pitch, roll=roll)
+            sky.phi_z = (2 * np.pi - phi) % (2 * np.pi) - np.pi
+            sky.theta_z = (3 * np.pi/2 - theta) % (2 * np.pi) - np.pi
+
         return sky
 
     @staticmethod
@@ -457,7 +467,11 @@ class SkyModel(object):
             theta, phi = hp.Rotator(rot=(0., np.rad2deg(pitch), 0.))(theta, phi)
         if not np.isclose(yaw, 0.):
             theta, phi = hp.Rotator(rot=(np.rad2deg(yaw), 0., 0.))(theta, phi)
-        return theta, (phi + np.pi) % (2 * np.pi) - np.pi
+        theta = (theta + np.pi) % (2 * np.pi) - np.pi
+        phi = (phi + np.pi) % (2 * np.pi) - np.pi
+        # if isinstance(theta, np.ndarray) and isinstance(phi, np.ndarray):
+        #     phi[theta > np.pi/2]
+        return theta, phi
 
 
 class BlackbodySkyModel(SkyModel):

tests/simulate-skylight.py
@@ -51,69 +51,89 @@ if __name__ == '__main__':
     print "Date: %s" % obs.date.datetime().strftime("%Y-%m-%d")
     print "Mode: %s" % ["luminance", "dop", "aop"][mode], " - nside = %d" % nside
 
+    x, y = np.arange(W), np.arange(H)
+    x, y = np.meshgrid(x, y)
+    x, y = x.flatten(), y.flatten()
+    pix = np.array([x, y])
+    theta, phi = pix2sph(pix, height=H, width=W)
+    j = np.all([~np.isnan(theta), ~np.isnan(phi)], axis=0)
+    x, y = x[j], y[j]
+    theta, phi = theta[j], phi[j]
+    sky = SkyModel(observer=obs, turbidity=tau, nside=1)
+    sky.get_features(theta, phi)
+
     while cur <= end:
         obs.date = cur
         sky = SkyModel(observer=obs, turbidity=tau, nside=1)
-        x, y = np.arange(W), np.arange(H)
-        x, y = np.meshgrid(x, y)
-        x, y = x.flatten(), y.flatten()
-        pix = np.array([x, y])
-        theta, phi = pix2sph(pix, height=H, width=W)
-        j = np.all([~np.isnan(theta), ~np.isnan(phi)], axis=0)
-        x, y = x[j], y[j]
-        theta, phi = theta[j], phi[j]
         sky.get_features(theta, phi)
-        sky = sky.rotate_sky(sky, yaw=np.pi/2, pitch=np.pi/6, roll=np.pi/3)
-        sky.generate()
+        # print np.rad2deg(sky.lon), np.rad2deg(sky.lat)
+        for k in xrange(3):
+            sky = sky.rotate_sky(sky, yaw=-k * np.pi / 6)
+            sky = sky.rotate_sky(sky, pitch=-k * np.pi / 18)
+            sky = sky.rotate_sky(sky, roll=-k * np.pi / 9)
+            sky = sky.rotate_sky(
+                sky,
+                yaw=(k + 1) * np.pi/6,
+                pitch=(k + 1) * np.pi/18,
+                roll=(k + 1) * np.pi/9
+            )
+            sky.generate()
 
-        if mode == 0:
-            sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
-            image = np.zeros((W, H, 3))
-            sky.DOP[np.isnan(sky.DOP)] = -1
-            i = np.argsort(sky.DOP)
-            # print sky.L.max()  # min(sky.L.max()) = 20, max(sky.L.max()) = 71
-            L = sky.L[i] / 20.
-            image[x[i], y[i], 0] = np.power(L, 1) + np.power(1. - L, 1) * .05  # deep sky blue * .53
-            image[x[i], y[i], 1] = np.power(L, 1) + np.power(1. - L, 1) * .53  # deep sky blue * .81
-            image[x[i], y[i], 2] = np.power(L, 1) + np.power(1. - L, 1) * .79  # deep sky blue * .92
-            image = np.clip(image, 0, 1)
-            plt.figure(1, figsize=(15, 15))
-            plt.imshow(image)
-            plt.xticks([])
-            plt.yticks([])
+            print "(%03d, %03d, %03d) - theta: %03d, phi: %3d" % (
+                np.rad2deg((k + 1) * np.pi/6),
+                np.rad2deg((k + 1) * np.pi/18),
+                np.rad2deg((k + 1) * np.pi/9),
+                np.rad2deg(sky.theta_z[0]), np.rad2deg(sky.phi_z[0])
+            )
 
-            # SkyModel.plot_luminance(sky, fig=1, title="Luminance", mode="00100", sub=(1, 1, 1))
-        elif mode == 1:
+            if mode == 0:
+                sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
+                image = np.zeros((W, H, 3))
+                sky.DOP[np.isnan(sky.DOP)] = -1
+                i = np.argsort(sky.DOP)
+                # print sky.L.max()  # min(sky.L.max()) = 20, max(sky.L.max()) = 71
+                L = sky.L[i] / 20.
+                image[x[i], y[i], 0] = np.power(L, 1) + np.power(1. - L, 1) * .05  # deep sky blue * .53
+                image[x[i], y[i], 1] = np.power(L, 1) + np.power(1. - L, 1) * .53  # deep sky blue * .81
+                image[x[i], y[i], 2] = np.power(L, 1) + np.power(1. - L, 1) * .79  # deep sky blue * .92
+                image = np.clip(image, 0, 1)
+                plt.figure(1, figsize=(15, 15))
+                plt.imshow(image)
+                plt.xticks([])
+                plt.yticks([])
 
-            sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
-            image = np.zeros((W, H, 3))
-            x, y = sph2pix(sph, W, H)
-            sky.DOP[np.isnan(sky.DOP)] = 0.
-            i = np.argsort(sky.DOP)
-            image[x[i], y[i], 0] = sky.DOP[i] * .53
-            image[x[i], y[i], 1] = sky.DOP[i] * .81
-            image[x[i], y[i], 2] = sky.DOP[i] * 1.0
-            plt.figure(1, figsize=(15, 15))
-            plt.imshow(image)
-            plt.xticks([])
-            plt.yticks([])
+                # SkyModel.plot_luminance(sky, fig=1, title="Luminance", mode="00100", sub=(1, 1, 1))
+            elif mode == 1:
 
-            # SkyModel.plot_polarisation(sky, fig=1, title="", mode="10", sub=(1, 1, 1))
-        elif mode == 2:
+                sph = np.stack([sky.theta_z, sky.phi_z, np.ones_like(sky.theta_z)])
+                image = np.zeros((W, H, 3))
+                x, y = sph2pix(sph, W, H)
+                sky.DOP[np.isnan(sky.DOP)] = 0.
+                i = np.argsort(sky.DOP)
+                image[x[i], y[i], 0] = sky.DOP[i] * .53
+                image[x[i], y[i], 1] = sky.DOP[i] * .81
+                image[x[i], y[i], 2] = sky.DOP[i] * 1.0
+                plt.figure(1, figsize=(15, 15))
+                plt.imshow(image)
+                plt.xticks([])
+                plt.yticks([])
 
-            image = np.zeros((W, H))
-            sky.DOP[np.isnan(sky.DOP)] = 0.
-            i = np.argsort(sky.DOP)
-            image[x[i], y[i]] = sky.AOP[i]
-            plt.figure(1, figsize=(15, 15))
-            plt.imshow(image, cmap="hsv")
-            plt.xticks([])
-            plt.yticks([])
+                # SkyModel.plot_polarisation(sky, fig=1, title="", mode="10", sub=(1, 1, 1))
+            elif mode == 2:
 
-            # SkyModel.plot_polarisation(sky, fig=1, title="", mode="01", sub=(1, 1, 1))
+                image = np.zeros((W, H))
+                sky.DOP[np.isnan(sky.DOP)] = 0.
+                i = np.argsort(sky.DOP)
+                image[x[i], y[i]] = sky.AOP[i]
+                plt.figure(1, figsize=(15, 15))
+                plt.imshow(image, cmap="hsv")
+                plt.xticks([])
+                plt.yticks([])
 
-        plt.draw()
-        plt.pause(.01)
+                # SkyModel.plot_polarisation(sky, fig=1, title="", mode="01", sub=(1, 1, 1))
+
+            plt.draw()
+            plt.pause(.01)
 
         # increase the current time
         cur = cur + delta