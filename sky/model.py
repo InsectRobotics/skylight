Fix bug in sensor - fix angle of polarisation rotation bug - fix the visualisation of the sensor - update sensor to work with tilted skies - change the computational model from linear to multilayer and non-linear - validate with sun position - update data-sets

sky/model.py
@@ -261,7 +261,7 @@ class SkyModel(object):
         if not self.is_generated:
             self.generate()
 
-        return (self.phi_s + np.pi / 2) % np.pi
+        return (self.phi_s + np.pi / 2) % (2 * np.pi)
 
     @property
     def E_par(self):